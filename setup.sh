675b89a22197bc2123128f343e6e6c8ae367df9d
Josef Stribny <jstribny@redhat.com>
2015-09-21 12:34:07 +0200

    Fix setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ sudo dnf install ruby-devel zlib-devel libxml2-devel libxslt-devel nodejs -y || 
 # ActionView is used for adjusting RSS feed
 sudo dnf install rubygem-actionview -y || :
 # spec dependencies for Capybara & Webkit testing
-sudo dnf install rubygem-rack rubygem-capybara rubygem-rspec
+sudo dnf install rubygem-rack rubygem-capybara rubygem-rspec -y || :
 # We need pre-release because of hooking rss.rb script into the build itself
 gem install jekyll --pre || :
 gem install nokogiri -- --use-system-libraries || :
