db42f02c2dd95e1c1ae0fc753b20977984358930
Josef Stribny <jstribny@redhat.com>
2015-10-09 13:49:15 +0200

    Update setup script to install rubygem-liquid

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # This script installs the necessary dependencies on Fedora
 
-sudo dnf group install "C Development Tools and Libraries" -y || :
-sudo dnf install ruby-devel zlib-devel libxml2-devel libxslt-devel nodejs -y || :
-# ActionView is used for adjusting RSS feed
-sudo dnf install rubygem-actionview -y || :
+sudo dnf group install "C Development Tools and Libraries" -y
+sudo dnf install ruby-devel zlib-devel libxml2-devel libxslt-devel nodejs -y
+# ActionView and liquid is used for RSS feed
+sudo dnf install rubygem-liquid rubygem-actionview -y
 # spec dependencies for Capybara & Webkit testing
-sudo dnf install rubygem-rack rubygem-capybara rubygem-rspec -y || :
+sudo dnf install rubygem-rack rubygem-capybara rubygem-rspec -y
 # We need pre-release because of hooking rss.rb script into the build itself
-gem install jekyll --pre || :
-gem install nokogiri -- --use-system-libraries || :
-gem install jekyll-lunr-js-search || :
-gem install jekyll-sitemap || :
+gem install jekyll --pre
+gem install nokogiri -- --use-system-libraries
+gem install jekyll-lunr-js-search
+gem install jekyll-sitemap
