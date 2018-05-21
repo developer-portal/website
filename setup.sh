6627abc00f25208f30afb58c0d1267c13cf02f72
Antonín Mrkvica <antonin.mrkvica@outlook.cz>
2018-05-21 12:20:28 +0200

    setup fixed

--- setup.sh
+++ setup.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 # This script installs the necessary dependencies on Fedora
+set -xe
 
 sudo dnf group install "C Development Tools and Libraries" -y
-sudo dnf install ruby-devel zlib-devel libxml2-devel libxslt-devel nodejs -y
 # FeedParser is used for RSS feed
-sudo dnf install python-feedparser -y
 # spec dependencies for Capybara & Webkit testing
-sudo dnf install rubygem-rack rubygem-capybara rubygem-rspec -y
-# We need Jekyll 3.0 because of hooking rss.py script into the build itself
-# but beta release because of compatibility with jekyll-lunr-js-search
-# plugin, see https://github.com/slashdotdash/jekyll-lunr-js-search/pull/85
+sudo dnf install -y ruby-devel zlib-devel libxml2-devel libxslt-devel nodejs rubygem-jekyll \
+  python-feedparser rubygem-rack rubygem-capybara rubygem-rspec rubygem-nokogiri redhat-rpm-config
+
 gem uninstall jekyll || :
-gem install jekyll --version 3.0.0.pre.beta10
-gem install nokogiri -- --use-system-libraries
 gem install jekyll-lunr-js-search
 gem install jekyll-sitemap
