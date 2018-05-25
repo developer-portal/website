54d72f28a1adeb65c42b9d2784a7f0010eb3d9f1
Pavel Valena <pvalena@redhat.com>
2018-05-25 12:01:33 +0200

    Add jekyll-email-protect gem
    also update submodule commit.

--- setup.sh
+++ setup.sh
@@ -5,9 +5,11 @@ set -xe
 sudo dnf group install "C Development Tools and Libraries" -y
 # FeedParser is used for RSS feed
 # spec dependencies for Capybara & Webkit testing
-sudo dnf install -y ruby-devel zlib-devel libxml2-devel libxslt-devel nodejs rubygem-jekyll \
-  python-feedparser rubygem-rack rubygem-capybara rubygem-rspec rubygem-nokogiri redhat-rpm-config
+sudo dnf install -y ruby-devel zlib-devel libxml2-devel libxslt-devel nodejs \
+  rubygem-jekyll python-feedparser rubygem-rack rubygem-capybara \
+  rubygem-rspec rubygem-nokogiri redhat-rpm-config
 
 gem uninstall jekyll || :
 gem install jekyll-lunr-js-search
 gem install jekyll-sitemap
+gem install jekyll-email-protect
