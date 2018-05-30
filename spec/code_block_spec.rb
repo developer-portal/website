d38693e6983c3bf3b173e83cce4f55da515db799
Antonín Mrkvica <antonin.mrkvica@outlook.cz>
2018-05-30 10:45:01 +0200

    rename variable "site" to "content", move comment, edit codes inside method extract_code, add Array of results from testing bash syntax

--- spec/code_block_spec.rb
+++ spec/code_block_spec.rb
@@ -1,17 +1,18 @@
 require 'spec_helper'
 require 'kramdown'
 require 'ap'
-require 'pp'
-site = File.join(File.dirname(__FILE__), '..','content', '**', '*.md')
-PAGES = Dir.glob(site)
+
+content = File.join(File.dirname(__FILE__), '..','content', '**', '*.md')
+PAGES = Dir.glob(content)
 
 def extract_code(element,prev_element)
-  codes = [] 
+  codes = []
+  #element value hasn't got any sibling or begin with '\n'
   if element.type == :codespan \
     && !element.value.nil? \
     && !codes.include?(element.value) \
-    && ((prev_element.children.size == 1) || (element.value[/^\n/])) #element value hasn't got any sibling or begin with '\n' 
-      codes.push element.value
+    && ((prev_element.children.size == 1) || (element.value[/^\n/]))
+      codes.push element.value.gsub "\n",''
   end
   element.children.each do |child|
     codes += extract_code(child, element)
@@ -22,25 +23,21 @@ end
 PAGES.each do |page|
   generated_page = Kramdown::Document.new(File.read(page))
   codes = extract_code(generated_page.root,generated_page)
-  
+  results = []
+
   describe 'Code blocks on markdown' do
 
-    
-    before :each do 
-      codes.each do |code|
-        code.gsub! "\n",''
-      end
-    end  
     it 'have good syntax' do
       codes.each do |code|
         if code[/^\$/] #code starts with '$'
-          expect(system('bash', '-n', '-c', code, '&>/dev/null')).to be_truthy,"Something wrong with: '#{code}'"
+          results.push([code,system('bash', '-n', '-c', code, "&>/dev/null")])
+          expect(results[-1][1]).to be_truthy,"Something wrong with: '#{code}'"
         elsif code[/^\#/] #code starts with '#'
           #ap code
         else #code starts with something else
           #ap code
         end
       end
-    end
+    end 
   end
 end
