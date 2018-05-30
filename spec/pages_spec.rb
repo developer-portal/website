3c7fafab62d62c1c20ef34c9aee7fe070b3c0876
Antonín Mrkvica <antonin.mrkvica@outlook.cz>
2018-05-30 14:01:31 +0200

    Edit code structure, delete debug code, separate "url_exists?" and "expect" methods

--- spec/pages_spec.rb
+++ spec/pages_spec.rb
@@ -6,7 +6,6 @@ PAGES = Dir.glob(site).map{ |p| p.gsub(/[^_]+\/_site(.*)/, '\\1') }
 
 urls = ['http://localhost:4000', 'http://localhost']
 status=''
-
 PAGES.each do |p|
   describe p do
     it_behaves_like 'Page'
@@ -16,14 +15,15 @@ PAGES.each do |p|
       visit p
     end
 
-    it 'has only valid internal hyperlinks' do
+    it 'has valid internal hyperlinks' do
       page.all(:css, 'a').each do |link|
-        next if link.text.nil? || link.text.empty? || link[:href].match(/^http.*/) || link.path.nil? || urls.include?(link[:href])
-        begin
-          page.find(:xpath, link.path).click
-	rescue NoMethodError
-	  require 'irb';binding.irb
-	end
+        #do not check link, which isn't clickable or hasn't got any path
+        next if link.text.nil?\
+          || link.text.empty?\
+          || link[:href].match(/^http.*/)\
+          || link.path.nil?\
+          || urls.include?(link[:href])
+        page.find(:xpath, link.path).click
         urls.push link.path
 	expect(page.status_code).to be(200), "expected link '#{link.text}' to work"
         visit p
@@ -32,9 +32,13 @@ PAGES.each do |p|
 
     it 'has valid external hyperlinks' do
       page.all(:css, 'a').each do |link|
-        next if link.text.nil? || link.text.empty? || !link[:href].match(/^http.*/) || urls.include?(link[:href])
+        next if link.text.nil?\
+          || link.text.empty?\
+          || !link[:href].match(/^http.*/)\
+          || urls.include?(link[:href])
 	urls.push(link[:href])
-	expect(url_exists? link[:href], status).to be_truthy, "expected link '#{link.text}' => '#{link[:href]}' to work (Error '#{status}')"
+        result = url_exists? link[:href], status
+	expect(result).to be_truthy, "expected link '#{link.text}' => '#{link[:href]}' to work (Error '#{status}')"
         visit p
       end
     end
