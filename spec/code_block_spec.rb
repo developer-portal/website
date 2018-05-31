f4cbfb54ab53db64c86a69b0f961da9e30d7c288
Pavel Valena <pvalena@redhat.com>
2018-05-31 14:17:56 +0200

    WIP: code_block_spec.rb refactoring
    and add comments.

--- spec/code_block_spec.rb
+++ spec/code_block_spec.rb
@@ -5,39 +5,57 @@ require 'ap'
 content = File.join(File.dirname(__FILE__), '..','content', '**', '*.md')
 PAGES = Dir.glob(content)
 
-def extract_code(element,prev_element)
-  codes = []
-  #element value hasn't got any sibling or begin with '\n'
-  if element.type == :codespan \
-    && !element.value.nil? \
-    && !codes.include?(element.value) \
-    && ((prev_element.children.size == 1) || (element.value[/^\n/]))
-      codes.push element.value.gsub "\n",''
+def extract_code(parent)
+  codespans = []
+  parent.children.each do |child|
+    
+    # Checks ... parent value hasn't got any sibling or begin with '\n'
+                                                                    #<<< TODO: ^^^ Why?
+    if child.type == :codespan \
+        && !child.value.nil? \                                      #<<< remove?, investigate
+        && !codespans.include?(child.value) \                       #<<< remove, keep context
+        && ((parent.children.size == 1) || (child.value[/^\n/]))    #<<< use start_with?
+      
+      codespans.push child.value.gsub "\n",' '                      #<<< .split and add to codespans
+                                                                    #<<< add .trim - white space in the beginning and end
+    end
+
+    codespans += extract_code(child)
   end
-  element.children.each do |child|
-    codes += extract_code(child, element)
-  end
-  codes
+  codespans
 end
 
 PAGES.each do |page|
-  generated_page = Kramdown::Document.new(File.read(page))
-  codes = extract_code(generated_page.root,generated_page)
-  results = []
+  generated_page = Kramdown::Document.new(File.read(page))          #<<< MD
+  codespans = extract_code(generated_page.root)                     #<<< lines of code, rename?
+  results = []                                                      #<<< to Hash: {codespan => result}
 
-  describe 'Code blocks on markdown' do
+  describe 'Command in Markdown file' do
 
-    it 'have good syntax' do
-      codes.each do |code|
+    it 'has a good syntax' do                                       #<<< passes syntax check
+      codespans.each do |code|
         if code[/^\$/] #code starts with '$'
-          results.push([code,system('bash', '-n', '-c', code, "&>/dev/null")])
+          results.push([code,system('bash', '-n', '-c', code, "&>/dev/null")])       #<<< do not execute here
           expect(results[-1][1]).to be_truthy,"Something wrong with: '#{code}'"
-        elsif code[/^\#/] #code starts with '#'
+        elsif code[/^\#/]                                           #<<< code.starts_with? ?#
           #ap code
         else #code starts with something else
           #ap code
         end
       end
-    end 
+    end
+    
+    it 'does not begin with #' do
+      skip
+    end
+    
+    it 'starts with $' do
+      skip
+    end
+    
+    it 'runs dnf with sudo' do
+      skip
+    end
+  
   end
 end
