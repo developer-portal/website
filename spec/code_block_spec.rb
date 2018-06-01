ceadc093880cc087f66f499a8357454833b69bd9
Antonín Mrkvica <antonin.mrkvica@outlook.cz>
2018-06-01 14:32:52 +0200

    rename variables, change extract method and extend error message
    rename PAGES to MD_FILES and codespans to code_lines, change extract method, add file path into decsribe error message, replace 'each' with 'inject'

--- spec/code_block_spec.rb
+++ spec/code_block_spec.rb
@@ -1,34 +1,47 @@
 require 'spec_helper'
 require 'kramdown'
-require 'ap'
 
 content = File.join(File.dirname(__FILE__), '..','content', '**', '*.md')
-PAGES = Dir.glob(content)
+MD_FILES = Dir.glob(content)
 
 def extract_code(parent)
-  codespans = []
+  code_lines = []
+
   parent.children.each do |child|
     # Checks if child.value hasn't got any sibling or begin with '\n', because only code is needed
     if child.type == :codespan \
       && ((parent.children.size == 1) || (child.value.start_with? ?\n))
-        codespans += child.value.strip().split("\n").map! { |x| x.strip()}
+        code_lines += child.value
+          .split(?\n)
+          .map(&:strip)
+          .inject([]) do |res, cu|
+            if !res.last.nil? && res.last.end_with?("\\")
+              res.last[0..-2].concat cu
+            else
+              res.push cu
+            end
+            res
+          end
     end
-    codespans += extract_code(child)
+
+    code_lines += extract_code child
   end
-  codespans
+  code_lines.reject(&:empty?)
 end
 
-PAGES.each do |page|
-  markdown_document = Kramdown::Document.new(File.read(page))
+MD_FILES.each do |file|
+  markdown_document = Kramdown::Document.new(File.read(file))
   code_lines = extract_code(markdown_document.root)
-  results = {}
-  code_lines.each do |code|
-    results[code] = system("bash", "-n", "-c", code, :err => File::NULL)
+
+  results = code_lines.inject({}) do |r, code|
+    r[code] = system("bash", "-n", "-c", code, :err => File::NULL)
+    r
   end
-  describe 'Command in Markdown file' do
-    results.keys.each do |code|
+
+  describe "Command in Markdown file '#{file.gsub(/[^_]+\/\.\.\/(content\/.*)/, '\\1')}'" do
+    results.each do |code, result|
       it 'has a good syntax' do
-        expect(results[code]).to be_truthy,"Something wrong with: '#{code}'"
+        expect(result).to be_truthy,"Something wrong with: '#{code}'"
       end
       next
       it 'does not begin with #' do
